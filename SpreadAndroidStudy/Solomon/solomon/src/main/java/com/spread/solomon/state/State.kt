efd183abb6a905831bb7cd243c912375f31cea47
SpreadZhao <www.spreadzhao@outlook.com>
2024-10-04 02:13:43 +0800

    delete unused

--- SpreadAndroidStudy/Solomon/solomon/src/main/java/com/spread/solomon/state/State.kt
+++
@@ -1,9 +0,0 @@
-package com.spread.solomon.state
-
-interface State {
-    fun getType(): Int
-    fun getLevel(): Int
-    fun getInfo(): Any?
-    fun name(): String?
-    fun levelName(): String?
-}
