d46cb8949cc5819132288715b6199980ee374894
SpreadZhao <www.spreadzhao@outlook.com>
2025-03-09 01:21:20 +0800

    Leetcode: 3 sum closest

--- Leetcode/LeetcodeCpp/Questions/Solution.cpp
+++ Leetcode/LeetcodeCpp/Questions/Solution.cpp
@@ -1307,3 +1307,34 @@ vector<int> Solution::twoSum2(vector<int> &nums, int target) {
     }
     return {};
 }
+
+int Solution::threeSumCloset(vector<int> &nums, int target) {
+    sort(nums.begin(), nums.end());
+    int diff = INT_MAX;
+    int res = INT_MAX;
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue;
+        }
+        int j = i + 1, k = nums.size() - 1;
+        while (j < k) {
+            const int sum = nums[i] + nums[j] + nums[k];
+            if (sum == target) {
+                return sum;
+            } else if (sum < target) {
+                if (abs(sum - target) < diff) {
+                    diff = abs(sum - target);
+                    res = sum;
+                }
+                j++;
+            } else {
+                if (abs(sum - target) < diff) {
+                    diff = abs(sum - target);
+                    res = sum;
+                }
+                k--;
+            }
+        }
+    }
+    return res;
+}
