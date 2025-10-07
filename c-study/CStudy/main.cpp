7f286e2876da9adc049de1bd0ef330a70ad139f1
SpreadZhao <spreadzhao@outlook.com>
2025-10-08 00:29:33 +0800

    multi-level pointer

--- c-study/CStudy/main.cpp
+++ c-study/CStudy/main.cpp
@@ -8,6 +8,11 @@ void test() {
 }
 
 int main() {
-    test();
+    int a = 1;
+    int *p1 = &a;
+    int **p2 = &p1;
+    int ***p3 = &p2;
+    int ****p4 = &p3;
+    std::printf("a: %d, p1: %p, p2: %p, p3: %p, p4: %p\n", a, p1, p2, p3, p4);
     return 0;
 }
